1d27b61ee5f338927a5c4a6f30bec92b5a57d1b9
muzaffer206 <ryuma.01d@gmail.com>
2024-03-12 10:01:51 +0300

    Proje dosyası ekle.

---
+++ Uygulama10U11/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Uygulama10U11
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sehirler = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirAdi.Text);
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            foreach (string sehir in sehirler)
+            {
+                lbListe.Items.Clear();
+                lbListe.Items.Add(sehir);
+            }
+        }
+    }
+}
